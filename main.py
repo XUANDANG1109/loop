c4812f6e118f22f0de268ac7e2fc45f584dc45b6
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-09-01 15:11:20 +0300

    aa

---
+++ main.py
@@ -0,0 +1,15 @@
+n = 1
+while n <= 5:
+    print(f"Value is {n:d}")
+    n = n + 1
+print("List is ready")
+
+# Phase 1
+n = 1
+while n <= 1000:
+    if n % 3 == 0:
+        print(str(n))
+    n = n+1
+
+
+
