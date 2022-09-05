fe371e0724aa2bb4dbaab98f727fa4273b3e3889
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-09-05 13:25:36 +0300

    aa

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ while n <= 5:
 print("List is ready")
 
 # Phase 1
-n = 1
+n = 0
 while n <= 1000:
     if n % 3 == 0:
         print(str(n))
     n = n+1
 
+# Phase 2
+number_inch = float(input("Input the numbers (inputs a negative value):"))
+while number_inch >= 0:
+    number_cm = round(number_inch * 2.54,2)
+    print("Converts inches to centimeters:", number_cm)
+    number_inch = float(input("Input the numbers (inputs a negative value):"))
+
+# Phase 3
+largest = 0
+number = float(input("Input the numbers:"))
 
 
