993398dc02e0e1fe01fb1cbe34215a2460f542c9
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-09-07 09:38:27 +0300

    aa

--- main.py
+++ main.py
@@ -9,17 +9,46 @@ n = 0
 while n <= 1000:
     if n % 3 == 0:
         print(str(n))
-    n = n+1
+    n = n + 1
 
 # Phase 2
 number_inch = float(input("Input the numbers (inputs a negative value):"))
 while number_inch >= 0:
-    number_cm = round(number_inch * 2.54,2)
+    number_cm = round(number_inch * 2.54, 2)
     print("Converts inches to centimeters:", number_cm)
     number_inch = float(input("Input the numbers (inputs a negative value):"))
 
 # Phase 3
 largest = 0
-number = float(input("Input the numbers:"))
+smallest = 0
+number = input("Input the numbers:")
+if number != "":
+    largest = float(number)
+    smallest = float(number)
+while number != "":
+    if float(number) > largest:
+        largest = float(number)
+    elif float(number) < smallest:
+        smallest = float(number)
+    number = input("Input the numbers:")
+print("The program prints out the largest", largest)
+print("The program prints out the smallest", smallest)
+
+# Phase 4
+import random
+random_number = random.randint(1,10)
+guess_number = random.randint(1,10)
+guess_number = int(input("Guess the right number from 1 to 10:"))
+while random_number != guess_number:
+    if guess_number > 10:
+        print("wrong number")
+    elif random_number < guess_number:
+        print("Too high!")
+    else:
+        print("Too low!")
+    guess_number = int(input("Guess the right number from 1 to 10:"))
+print("Correct")
+
+
 
 
