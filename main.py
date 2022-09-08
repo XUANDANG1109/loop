b95bdbe6165aba2b628ca666259b9285a68ea022
Xuan Dang <102602490+XUANDANG1109@users.noreply.github.com>
2022-09-08 14:47:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 1
-while n <= 5:
-    print(f"Value is {n:d}")
-    n = n + 1
-print("List is ready")
+# NAME: XUAN DANG
 
 # Phase 1
 n = 0
@@ -79,3 +75,69 @@ while number > count:
     count = count + 1
 a = 4 * inside_circle / number
 print("The approximate value of pi: ", a)
+
+# Phase 2 solution
+prompt = "Give me inches"
+inch = float(input(prompt))
+while inch >= 0:
+    print(f"It is in centimeters: {inch*2.54:.1f}")
+    inch = float(input(prompt))
+
+# Phase 3 solution
+prompt = "Give a number"
+s = input(prompt)
+smallest = int(s)
+largest = smallest
+while s!= "":
+    n = int(s)
+    if n<smallest:
+        smallest = n
+    if n > largest:
+        largest = n
+    s = input(prompt)
+else:
+    print(f"The smallest number given was: {smallest}, amd the largest was {largest}")
+
+# Phase 4
+import random
+the_number = random.randint(1,10)
+prompt = "Try to guess the number"
+guess = int(input(prompt))
+while guess != the_number:
+    if guess > the_number:
+        print("Too high")
+    else:
+        print("Too low")
+    guess = int(input(prompt))
+else:
+    print("Correct")
+
+# Phase 5
+username = "python"
+password = "rules"
+n = 5
+while n > 0:
+    u = input("Give the username:")
+    p = input("Give the passport:")
+    if u == username and p == password:
+        print("Welcome")
+        break
+    n = n -1
+else:
+    print("Access denied")
+
+# Phase 6
+import math
+import random
+N = int(input("How many random points to generate?"))
+n = 0
+i = 0
+while i < N :
+    x = random.uniform(-1.,1.)
+    y = random.uniform(-1.,1.)
+
+    if x**2 + y**2 <1.:
+        n = n + 1
+    i = i + 1
+pi = 4*n/N
+print(f"Pi is {pi}, error {math.pi - pi}")
